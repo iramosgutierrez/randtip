3e2db3714a1c9b669584f05a786a00febe395f3c
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-11 18:20:11 +0100

    add.to.paraphyletic changes
    automatization given intern and groups nodes or not, some changes

--- R/add_to_paraphyletic.R
+++ R/add_to_paraphyletic.R
@@ -1,79 +1,81 @@
-#' Same process than paraphyletic node, bt detecting automatically the deep and shallow nodes
-#' @param group.node mrca node of the paraphyletic clade. If no intern.node is supplied, a random 
-#'                    internal node is selected
-#' @param intern.node node of the clade to insert the new tip. It must be used in conjuction 
+#' Insert in paraphyletic. i no nodes are given, they will be automatically searched.
+#' @param group.node mrca node of the paraphyletic clade. It must be used in conjuction
+#'                    with intern node
+#' @param intern.node mrca node of the clade where not to insert the new tip. It must be used in conjuction
 #'                     with group node
 #' @export
 #' @examples
 #' #add.to.paraphyletic(Tree, "Achillea_3_inventatus")
+
 add.to.paraphyletic <- function(tree, new.tip, prob = TRUE, intern.node = NULL,
                                 group.node = NULL){
-  
-  if(is.null(group.node) && !is.null(intern.node)){
-      stop("The group.node argument must be supplied along with intern.node argument.")
+
+  if((is.null(group.node) && !is.null(intern.node)) | (!is.null(group.node) && is.null(intern.node))){
+    stop("The group.node argument must be supplied along with intern.node argument.")
   }
-    
-  new.tip <- gsub(" ", "_", new.tip) 
-  
-  genus <- stringr::word(new.tip, 1, sep="_")
-  
+
+
+  if(is.null(group.node) && is.null(intern.node)){
+
+  new.tip <- gsub(" ", "_", new.tip)
+  genus <- randtip::firstword(new.tip)
+
   taxa.vector<- sp.genus.in.tree(tree, genus)
   if(length(taxa.vector) == 0){
-      stop(paste0("Genus ", genus, " is no included in yout tree."))
-  }
-    
-  sp <- tree$tip.label
-  if(!is.null(group.node)){
-      mrca <- group.node
-      descs <- phytools::getDescendants(tree, group.node)
-  }else{
-      mrca <- phytools::findMRCA(tree = tree, tips = taxa.vector)
-      descs <- phytools::getDescendants(tree, mrca)
-  }
-  descs.tips <- sp[phytools::getDescendants(tree, mrca)]
-  descs.tips <- descs.tips[!is.na(descs.tips)]
-  intruder.tips <- descs.tips[!stringr::str_detect(descs.tips, genus)]
-    
-  if((length(intruder.tips) == 0) & is.null(intern.node)){
-      stop(paste0("Genus ", genus, " is not paraphyletic. It is monophyletic."))
-  }
-  
-  if((length(intruder.tips) == 1) & is.null(intern.node)){
-      new.tree <- add.into.node(tree = tree, node = mrca, new.tip = new.tip)
-  }
-  
-  if(length(intruder.tips) > 1 | !is.null(intern.node)){
-    if(!is.null(intern.node)){
-        intruder.descs <- phytools::getDescendants(tree, intern.node)
-        intruder.descs <- intruder.descs[!is.na(intruder.descs)]
-    }else{
-        intruder.mrca <- phytools::findMRCA(tree, tips = intruder.tips)
-        intruder.descs <- phytools::getDescendants(tree, intruder.mrca)
-        intruder.descs <- intruder.descs[!is.na(intruder.descs)]
-        if(any(stringr::str_detect(sp[intruder.descs], genus))){
-            stop(paste0("Genus ", genus, 
-                        " is not paraphyletic. It is polyphyletic."))
-        }
+    stop(paste0("Genus ", genus, " is no included in your tree."))
     }
 
-    edge.length <- tree$edge.length
-    df <- data.frame(tree$edge, edge.length, 1:length(edge.length)) 
+  sp <- tree$tip.label
+
+    mrca <- phytools::findMRCA(tree = tree, tips = taxa.vector)
+    descs <- phytools::getDescendants(tree, mrca)
+
+  descs.tips <- sp[phytools::getDescendants(tree, mrca)]
+  descs.tips <- randtip::notNA(descs.tips)
+  intruder.tips <- descs.tips[firstword(descs.tips)!=genus]
+
+  if(length(intruder.tips) == 0){
+    stop(paste0("Genus ", genus, " is not paraphyletic. It is monophyletic."))
+  }
+
+  if(length(intruder.tips) == 1 ){
+    new.tree <- add.into.node(tree = tree, node = mrca, new.tip = new.tip)
+  }
+
+  if(length(intruder.tips) > 1 ){
+
+      intruder.mrca <- phytools::findMRCA(tree, tips = intruder.tips)
+      intruder.descs <- phytools::getDescendants(tree, intruder.mrca)
+      intruder.desc.names<- tree$tip.label[intruder.descs]
+      intruder.desc.names <- randtip::notNA(intruder.desc.names)
+      if(any(randtip::firstword(intruder.desc.names)==genus)){
+        stop(paste0("Genus ", genus,
+                    " is not paraphyletic. It is polyphyletic."))
+      }
+
+  }}else{
+      descs<-phytools::getDescendants(tree = tree, node = group.node)
+      intruder.descs<-phytools::getDescendants(tree = tree, node = intern.node)
+    }
+
+
+    df <- data.frame("parent.node"=tree$edge[,1],"child.node"= tree$edge[,2],"length"=tree$edge.length, "id" =1:length(edge.length))
     df <- df[df[,2] %in% descs,]
     df <- df[!(df[,2] %in% intruder.descs),]
     if(prob == TRUE){
-        to.index <- get.index(tree, how = "sample_prob", df = df)
+      to.index <- randtip::get.index(tree, how = "sample_prob", df = df)
     }else{
-        to.index <- get.index(tree)
+      to.index <- randtip::get.index(tree)
     }
-    where <- tree$edge[to.index, 2] 
-      
-      
-    tip.pos <- bind.tip.pos(pos.min = 0, 
+    where <- tree$edge[to.index, 2]
+
+
+    tip.pos <- randtip::bind.tip.pos(pos.min = 0,
                             pos.max = edge.length[to.index])
-   
-    new.tree <- phytools::bind.tip(tree, new.tip, edge.length = NULL, 
-                         where = where, position = tip.pos)
-   }
-  
-    return(new.tree)
+
+    new.tree <- phytools::bind.tip(tree, new.tip, edge.length = NULL,
+                                   where = where, position = tip.pos)
+
+
+  return(new.tree)
 }
