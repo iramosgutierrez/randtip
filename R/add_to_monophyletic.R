853c7cfd4d32f8637f3eac5d3a6cdb861de44b72
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-11 13:33:33 +0100

    add.to.monophyletic modification
    search of genus taxa in tree based on word 1 and not in detect (to avoid genus names in specific epithets)

--- R/add_to_monophyletic.R
+++ R/add_to_monophyletic.R
@@ -3,18 +3,18 @@
 #' @examples
 #' #plot(add_to_monophyletic(Tree, "Invent_inventata"))
 add.to.monophyletic <- function(tree, new.tip){
-    
-  new.tip <- gsub(" ", "_", new.tip)    
-  
-  genus <- stringr::word(new.tip, 1, sep = "_")     
-  
+
+  new.tip <- gsub(" ", "_", new.tip)
+
+  genus <- stringr::word(new.tip, 1, sep = "_")
+
   sp <- tree$tip.label
-  taxa.vector <- sp[stringr::str_detect(sp, genus)]    
+  taxa.vector <- sp[stringr::word(sp, 1, sep="_")==genus]
   if(length(taxa.vector) == 0){
       stop(paste0("Genus ", genus, " is not included in yout tree."))
   }
-    
-  mrca <- phytools::findMRCA(tree = tree, tips = taxa.vector) 
-  new.tree <- add.into.node(tree = tree, node = mrca, new.tip = new.tip) 
+
+  mrca <- phytools::findMRCA(tree = tree, tips = taxa.vector)
+  new.tree <- add.into.node(tree = tree, node = mrca, new.tip = new.tip)
   return(new.tree)
 }
