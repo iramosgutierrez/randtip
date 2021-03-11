32b6d7f02bf1b714e3e49af4d5e6854d19a2cd5b
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-11 12:02:21 +0100

    description and visualize_tree
    description: depend on stringdist
    
    visualizetrees: include packages phytoolls & stringr

--- R/visualize_tree.R
+++ R/visualize_tree.R
@@ -6,11 +6,11 @@ plotTree.MDCC<- function(tree, DF1, MDCC, DF2=NULL){
   if(!is.null(DF2)){spss2<- DF2[which(MDCC==DF2$MDCC),]}
   genera<- unique(spss$genus)
   if(!is.null(DF2)){genera<- unique(c(spss$genus, spss2$genus)) }
-  cut.list<- tree$tip.label[word(tree$tip.label, 1, sep="_")%in% genera]
+  cut.list<- tree$tip.label[stringr::word(tree$tip.label, 1, sep="_")%in% genera]
   if(length(cut.list)==0){stop("Specified MDCC is not reflected in the tree!")}
   if(length(cut.list)==1){stop("Specified MDCC is related to just 1 tree tip!")}
   cut.node<- ape::getMRCA(tree, tip =cut.list )
-  subtree<-  splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
+  subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
   return(plot(subtree))
 }
 #plotTree.MDCC(tree25, DF1 =DF1, MDCC="Pinaceae")
@@ -24,11 +24,11 @@ cutTree.MDCC<- function(tree, DF1, MDCC, DF2=NULL){
   if(!is.null(DF2)){spss2<- DF2[which(MDCC==DF2$MDCC),]}
   genera<- unique(spss$genus)
   if(!is.null(DF2)){genera<- unique(c(spss$genus, spss2$genus)) }
-  cut.list<- tree$tip.label[word(tree$tip.label, 1, sep="_")%in% genera]
+  cut.list<- tree$tip.label[stringr::word(tree$tip.label, 1, sep="_")%in% genera]
   if(length(cut.list)==0){stop("Specified MDCC is not reflected in the tree!")}
   if(length(cut.list)==1){stop("Specified MDCC is related to just 1 tree tip!")}
   cut.node<- ape::getMRCA(tree, tip =cut.list )
-  subtree<-  splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
+  subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
   return((subtree))
 }
 #cutTree.MDCC(tree, DF1, MDCC="Clinopodium")
