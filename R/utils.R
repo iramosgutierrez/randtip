35f919efa6a3bbbc51b0c45b652a475247af4785
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-11 17:49:09 +0100

    firstword function created
    randtip::firstword ()= stringr::word(string, 1, sep=sep)

--- R/utils.R
+++ R/utils.R
@@ -10,7 +10,7 @@ sp.genus.in.tree <- function(tree, genus){
 # The returning value is to be used in the position argument
 # of phytools bind.tip function.
 bind.tip.pos <- function(pos.min, pos.max){
-     pos.tip <- stats::runif(1, pos.min, pos.max)    
+     pos.tip <- stats::runif(1, pos.min, pos.max)
      while(pos.tip == pos.min | pos.tip == pos.max){
         pos.tip <- stats::runif(1, pos.min, pos.max)
      }
@@ -22,38 +22,38 @@ get.index <- function(tree, how = "sample_simple", node = NULL, df = NULL){
     if(is.null(df)){
         df <- data.frame(tree$edge, tree$edge.length, 1:length(tree$edge.length))
     }
-    
-    if(!is.null(node)){        
+
+    if(!is.null(node)){
         edges <- df[ape::which.edge(tree, node),]
     }else if(how == "sample_simple"){
         edges <- df[sample(x = 1:nrow(df), size = 1),]
     }else if(how == "sample_prob"){
-        edges <- df[sample(x = 1:nrow(df), size = 1, prob = df[,3]),]            
+        edges <- df[sample(x = 1:nrow(df), size = 1, prob = df[,3]),]
     }else{
         stop("Unrecognized set of arguments to get.index function.")
     }
-    
-    to.index <- edges[,4] 
-    
+
+    to.index <- edges[,4]
+
     return(to.index)
 }
 
 get.position <- function(tree, node, insertion){
-    
+
     edge.length <- tree$edge.length[ape::which.edge(tree, node)]
-    if(insertion=="random"){ 
+    if(insertion=="random"){
         # Bind at a random point of the branch
-        position <- edge.length * stats::runif(1, 0, 1) 
+        position <- edge.length * stats::runif(1, 0, 1)
     }else if(insertion=="middle"){
         position <- edge.length*0.5
-        # Bind at the middle of the branch    
+        # Bind at the middle of the branch
     }else if(insertion=="long"){
         # Bind at the beggining of the branch
         position <- edge.length
     }else{
         stop("Unknown specification of the insertion argument.")
     }
-    
+
     return(position)
 }
 
@@ -61,31 +61,31 @@ get.position <- function(tree, node, insertion){
 get.parent.siblings <- function(tree, tip){
     tree.sp <- tree$tip.label
     # Direct ancestor
-    parent <- tree$edge[tree$edge[,2] == tip, 1] 
+    parent <- tree$edge[tree$edge[,2] == tip, 1]
     # Ancestor's descendants
     parent.desc <- phytools::getDescendants(tree, parent)
     siblings <- tree.sp[parent.desc]
-    siblings <- siblings[!is.na(tree.sp[parent.desc])] 
-    
-    return(list(parent = parent,  
+    siblings <- siblings[!is.na(tree.sp[parent.desc])]
+
+    return(list(parent = parent,
                 siblings = siblings))
 }
 
 par.sib.diff.genera <- function(tree, siblings.genera, tip){
     # At least one sibling is from the same genus
-    while(length(unique(siblings.genera)) == 1){ 
+    while(length(unique(siblings.genera)) == 1){
         # Tip and parent upstream until they are from different genera
-        par.sib <- get.parent.siblings(tree, tip) 
+        par.sib <- get.parent.siblings(tree, tip)
         siblings.genera <- stringr::word(par.sib$siblings, 1, sep = "_")
         tip <- par.sib$parent
     }
-    
+
     return(par.sib)
 }
 
 sib.intruders <- function(tree, genus, siblings){
     sibling.genera <- stringr::word(siblings, 1, sep = "_")
-    
+
     if(length(siblings[sibling.genera != genus]) == 1){
         siblings <- siblings[sibling.genera == genus]
     }else if(length(siblings[sibling.genera != genus]) > 1){
@@ -96,30 +96,30 @@ sib.intruders <- function(tree, genus, siblings){
         intr.desc.names<- intr.desc.names[!is.na(intr.desc.names)]
         intr.genera <- stringr::word(intr.desc.names, 1, sep = "_")
         if(all(intr.genera != genus)){
-            siblings<-siblings[sibling.genera==genus] 
+            siblings<-siblings[sibling.genera==genus]
         }
     }
-        
+
     return(siblings)
 }
 
 
 get.grouped <- function(tree, siblings.genera, genus, tip){
-    
+
     par.sib <- par.sib.diff.genera(tree, siblings.genera, tip)
     siblings <- par.sib$siblings
     siblings.genera <- stringr::word(siblings, 1, sep = "_")
-    gen.mrca <- phytools::findMRCA(tree = tree, 
+    gen.mrca <- phytools::findMRCA(tree = tree,
                                   tips = siblings[siblings.genera == genus])
     gen.mrca.desc <- phytools::getDescendants(tree, gen.mrca)
     # Non-node descendants
-    grouped <- tree$tip.label[gen.mrca.desc][!is.na(gen.mrca.desc)] 
+    grouped <- tree$tip.label[gen.mrca.desc][!is.na(gen.mrca.desc)]
     # MRCA descendants
     grouped.gen <- stringr::word(grouped, 1, sep = "_")
-    
+
     return(list(grouped = grouped,
                 grouped.gen = grouped.gen,
-                gen.mrca = gen.mrca))    
+                gen.mrca = gen.mrca))
 }
 
 # Convenience function to add species to node according to group and given
@@ -136,15 +136,21 @@ add.phyletic.group <- function(tree, sp, genus, grouped, grouped.gen, mrca){
         if(length(intruders) == 1){
             # Singleton intruders; added as monophyletic
             new.tree <- add.into.node(tree, mrca, sp)
-        }else{             
+        }else{
             # Monophyletic intruders; added as paraphyletic
             intruders.mrca <- phytools::findMRCA(tree = tree, tips = intruders)
-            new.tree <- add.to.paraphyletic(tree, 
-                                            new.tip = sp, 
-                                            group.node = mrca, 
-                                            intern.node = intruders.mrca) 
+            new.tree <- add.to.paraphyletic(tree,
+                                            new.tip = sp,
+                                            group.node = mrca,
+                                            intern.node = intruders.mrca)
         }
     }
 
     return(new.tree)
+}
+
+
+firstword<- function(string, sep="_"){
+  word<- stringr::word(string, 1, sep=sep)
+  return(word)
 }
