15f797451c84e370a78136f67dbacbe450b62405
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-15 15:10:24 +0100

    polytomy.into.node
    multiple length vectors usable now

--- R/polytomy_into_node.R
+++ R/polytomy_into_node.R
@@ -1,15 +1,38 @@
 #' @export
 polytomy.into.node <- function(tree, new.tip, node){
 
-  new.tree <- tree 
+  new.tree <- tree
+  node.descs<- phytools::getDescendants(tree=new.tree, node = node)
+  tip.descs<-new.tree$tip.label[node.descs]
+  tip.descs<-randtip::notNA(tip.descs)
 
-  to.index <- get.index(new.tree, node = node)
-  bind.where <- new.tree$edge[to.index, 2]   
-  # Indexing position; node length is 0
-  new.tree <- phytools::bind.tip(new.tree, 
-                                 new.tip, 
-                                 edge.length = NULL, 
-                                 where = bind.where, 
-                                 position = 0) 
+  for(i in 1: length(new.tip)){
+
+    if (i==1) {
+    to.index <- get.index(new.tree, node = node)
+    bind.where <- new.tree$edge[to.index, 2]
+    # Indexing position; node length is 0
+    new.tree <- phytools::bind.tip(new.tree,
+                                   new.tip[i],
+                                   edge.length = NULL,
+                                   where = bind.where,
+                                   position = 0)
+    }else{
+     sticksp<-  c(tip.descs,new.tip[1:i-1] )
+     node<- phytools::findMRCA(tree=new.tree, tips=sticksp)
+
+     to.index <- get.index(new.tree, node = node)
+     bind.where <- new.tree$edge[to.index, 2]
+     # Indexing position; node length is 0
+     new.tree <- phytools::bind.tip(new.tree,
+                                    new.tip[i],
+                                    edge.length = NULL,
+                                    where = bind.where,
+                                    position = 0)
+                                   }
+
+  }
+
+
   return(new.tree)
 }
