99eece322212e717462162dd4a9aba7fede680b1
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-15 15:24:19 +0100

    polytomy.over.node modifications
    homologous to polytomy.to.node

--- R/polytomy_over_node.R
+++ R/polytomy_over_node.R
@@ -1,31 +1,39 @@
 #' @export
-polytomy.over.node<- function(tree, node, species, 
-                              insertion = c("random", "long", "middle")){
+polytomy.over.node<- function(tree, node, new.tip,
+                              insertion = "random") { #insertion "random" or "middle" or "long"
 
-    new.tree <- tree 
-    for(i in 1:length(species)){
-        # First species is added to singleton
-        if(i==1){   
-            to.index <- get.index(new.tree, node = node)
-            bind.where <- new.tree$edge[to.index,2]
-            
-            position <- get.position(new.tree, node, insertion)
-    
-            new.tree <- phytools::bind.tip(new.tree, 
-                                           tip.label =  species[i], 
-                                           edge.length = NULL, 
-                                           where = bind.where, 
-                                           position = position)
-        }else{
-            # The rest of the species are added to the node
-            child.node <- which(new.tree$tip.label == species[i-1])
-            node <- phytools::getParent(tree = new.tree, 
-                                        node = child.node)
-                
-            new.tree <- polytomy.into.node(tree = new.tree, 
-                                          new.tip = species[i], 
-                                          node = node)
-        }
+  new.tree <- tree
+  node.descs<- phytools::getDescendants(tree=new.tree, node = node)
+  tip.descs<-new.tree$tip.label[node.descs]
+  tip.descs<-randtip::notNA(tip.descs)
+
+  for(i in 1: length(new.tip)){
+
+    if (i==1) {
+      to.index <- get.index(new.tree, node = node)
+      bind.where <- new.tree$edge[to.index, 2]
+      position<- get.position(tree=new.tree, node = 31, insertion = insertion)
+      new.tree <- phytools::bind.tip(new.tree,
+                                     new.tip[i],
+                                     edge.length = NULL,
+                                     where = bind.where,
+                                     position = position)
+    }else{
+      sticksp<-  c(tip.descs,new.tip[1:i-1] )
+      node<- phytools::findMRCA(tree=new.tree, tips=sticksp)
+
+      to.index <- get.index(new.tree, node = node)
+      bind.where <- new.tree$edge[to.index, 2]
+      # Indexing position; node length is 0
+      new.tree <- phytools::bind.tip(new.tree,
+                                     new.tip[i],
+                                     edge.length = NULL,
+                                     where = bind.where,
+                                     position = 0)
     }
-    return(new.tree)}
 
+  }
+
+
+  return(new.tree)
+}
