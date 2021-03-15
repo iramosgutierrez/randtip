11d3f607d6b7b2f239859663dea88866428c6cc2
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-15 14:38:04 +0100

    add.to.polyphiletic corrections
    some corrections. May include errors, but I don´t know where!!!

--- R/add_to_polyphyletic.R
+++ R/add_to_polyphyletic.R
@@ -25,48 +25,83 @@ add.to.polyphyletic <- function(tree, new.tip, polyphyletic.insertion = "freq", 
     }
 
     if(polyphyletic.insertion == "freq"){
-        for(sticked.species in new.tip){
 
 
-            # Choose a species at random
-            # Allows a probability based on frequencies inside each group
-            new.tree.sp <- new.tree$tip.label
+      groups <- rep(list(NA), times = length(taxa.vector))
+      names(groups) <- taxa.vector
+      group.types <- rep(list(NA), times = length(taxa.vector))
+      names(group.types) <- taxa.vector
 
-            sticking.species<- sample(taxa.vector, 1)
-            sticking.tip <- which(new.tree.sp == sticking.species)
+      for(t in 1:length(taxa.vector)){
+        taxon <- taxa.vector[t]
+        taxon.tip <- which(new.tree$tip.label == taxon)
+        par.sib <- get.parent.siblings(new.tree, taxon.tip)
 
-            par.sib <- get.parent.siblings(new.tree, sticking.tip)
+        siblings.genus <- randtip::firstword(par.sib$siblings)
 
-            siblings.genus <- randtip::firstword(par.sib$siblings)
-
-            if(sum(siblings.genus == genus) == 1){
-                # Siblings are from a different genus
-                # It is a singleton inside another clade
-                new.tree <- add.to.singleton(new.tree, sticking.species, new.tips = sticked.species)
-            }else if(sum(siblings.genus == genus) > 1){
-                grouped.list <- get.grouped(new.tree, siblings.genus,
-                                            genus, tip = par.sib$parent)
-
-                gen.mrca <- grouped.list$gen.mrca
-
-                grouped <- grouped.list$grouped
-                grouped.gen <- grouped.list$grouped.gen
-
-                # Add to tree according to phyletic group
-                new.tree <- add.phyletic.group(new.tree, sticked.species,
-                                               genus, grouped,
-                                               grouped.gen, gen.mrca)
-            }
+        if(sum(siblings.genus == genus) == 1){
+          groups[[t]]<- taxon
+          group.types[[t]]<- "singleton"
         }
-    }
+        if(sum(siblings.genus == genus) > 1){
+          grouped.list <- get.grouped(new.tree, siblings.genus,
+                                      genus, tip = par.sib$parent)
+
+          grouped <-notNA(  grouped.list$grouped)
+          grouped.gen <- notNA(grouped.list$grouped.gen)
+          grouped.gen.uniq <- notNA(unique(grouped.gen))
+
+          if(length(grouped.gen.uniq) == 1){
+            groups[[t]] <- grouped
+            group.types[[t]] <- "monophyletic"
+          }
+          if(length(grouped.gen.uniq) > 1){
+            groups[[t]] <- grouped[grouped.gen.uniq == genus]
+            group.types[[t]]<- "paraphyletic"
+          }
+        }
+      }
+
+
+      for(sp in new.tip){
+        randsp<- sample(x = taxa.vector, size = 1)
+        slot<- grep(randsp, x = groups)
+        if(length(slot)>1){slot<-sample(x=slot, size = 1)}
+        group <- groups[[slot]]
+        group.type <- group.types[[slot]]
+
+        if(group.type == "singleton"){
+           new.tree <- randtip::add.to.singleton(tree = new.tree, singleton = group, new.tips = sp)}
+
+        if(group.type == "monophyletic"){
+          mrca <- phytools::findMRCA(new.tree, tips = group)
+          new.tree <- randtip::add.into.node(tree = new.tree, node = mrca,
+                                             new.tip = sp, prob=prob)}
+        if(group.type == "paraphyletic"){
+          mrca <- phytools::findMRCA(new.tree, tips = group)
+          mrca.desc <- phytools::getDescendants(new.tree, mrca)
+          mrca.desc <- mrca.desc[!is.na(mrca.desc)]
+          mrca.desc.gen <- stringr::word(mrca.desc, 1, sep = "_")
+          intruders <- mrca.desc[mrca.desc.gen != genus]
+          intruders.mrca <- phytools::findMRCA(new.tree, tips = intruders)
+          new.tree <- add.to.paraphyletic(tree = new.tree,
+                                          new.tip = sp,
+                                          group.node = mrca,
+                                          intern.node = intruders.mrca, prob=prob)}
+      }
+      }
 
     if(polyphyletic.insertion == "large"){
-        groups <- rep(list(NA), times = length(taxa.vector))
-        names(groups) <- taxa.vector
-        group.types <- rep(list(NA), times = length(taxa.vector))
-        names(group.types) <- taxa.vector
 
-        for(t in 1:length(taxa.vector)){
+
+        for(sp in new.tip){
+
+          groups <- rep(list(NA), times = length(taxa.vector))
+          names(groups) <- taxa.vector
+          group.types <- rep(list(NA), times = length(taxa.vector))
+          names(group.types) <- taxa.vector
+
+          for(t in 1:length(taxa.vector)){
             taxon <- taxa.vector[t]
             taxon.tip <- which(new.tree$tip.label == taxon)
             par.sib <- get.parent.siblings(new.tree, taxon.tip)
@@ -74,38 +109,42 @@ add.to.polyphyletic <- function(tree, new.tip, polyphyletic.insertion = "freq", 
             siblings.genus <- randtip::firstword(par.sib$siblings)
 
             if(sum(siblings.genus == genus) == 1){
-                groups[[t]]<- taxon
-                group.types[[t]]<- "singleton"
+              groups[[t]]<- taxon
+              group.types[[t]]<- "singleton"
             }
             if(sum(siblings.genus == genus) > 1){
-                grouped.list <- get.grouped(new.tree, siblings.genus,
-                                            genus, tip = par.sib$parent)
+              grouped.list <- get.grouped(new.tree, siblings.genus,
+                                          genus, tip = par.sib$parent)
 
-                grouped <- grouped.list$grouped
-                grouped.gen <- grouped.list$grouped.gen
-                grouped.gen.uniq <- unique(grouped.gen)
+              grouped <-notNA(  grouped.list$grouped)
+              grouped.gen <- notNA(grouped.list$grouped.gen)
+              grouped.gen.uniq <- notNA(unique(grouped.gen))
 
-                if(length(grouped.gen.uniq) == 1){
-                    groups[[t]] <- grouped
-                    group.types[[t]] <- "monophyletic"
-                }
-                if(length(grouped.gen.uniq) > 1){
-                    groups[[t]] <- grouped[grouped.gen.uniq == genus]
-                    group.types[[t]]<- "paraphyletic"
-                }
+              if(length(grouped.gen.uniq) == 1){
+                groups[[t]] <- grouped
+                group.types[[t]] <- "monophyletic"
+              }
+              if(length(grouped.gen.uniq) > 1){
+                groups[[t]] <- grouped[grouped.gen.uniq == genus]
+                group.types[[t]]<- "paraphyletic"
+              }
             }
-        }
+          }
 
-        groups.sz <- lengths(groups)
-        max.sz <- max(as.numeric(groups.sz))
-        group.types <- group.types[groups.sz == max.sz]
-        groups <- groups[groups.sz == max.sz]
+          groups.sz <- lengths(groups)
+          max.sz <- max(as.numeric(groups.sz))
+          group.types <- group.types[groups.sz == max.sz]
+          groups <- groups[groups.sz == max.sz]
 
-        slot <- sample(1:length(groups), size = 1)
-        group <- groups[[slot]]
-        group.type <- group.types[[slot]]
+          slot <- sample(1:length(groups), size = 1)
+          group <- groups[[slot]]
+          group.type <- group.types[[slot]]
 
-        for(sp in new.tip){
+
+
+          if(group.type == "singleton"){
+            new.tree <- randtip::add.to.singleton(tree = new.tree, singleton = group, new.tips = sp)}
+
             if(group.type == "monophyletic"){
                 mrca <- phytools::findMRCA(new.tree, tips = group)
                 new.tree <- randtip::add.into.node(tree = new.tree, node = mrca,
@@ -128,3 +167,6 @@ add.to.polyphyletic <- function(tree, new.tip, polyphyletic.insertion = "freq", 
     return(new.tree)
 
 }
+
+#POR ALGUNA RAZÓN, CUANDO APLICO ESTA FUNCIÓN (EN FREQ O LARGE), A VECES INCLUYE UN NEW.TIP
+#AL FINAL DEL TODO, JUNTO A ACHILLUM MILLEFOLIUM
