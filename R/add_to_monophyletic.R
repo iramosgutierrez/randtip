10ff015bf10d92abd3770330f42933ba74287beb
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-03-25 11:18:10 +0100

    add.to.monophyletic correction
    tree correction in order to avoid a repeating error

--- R/add_to_monophyletic.R
+++ R/add_to_monophyletic.R
@@ -3,7 +3,7 @@
 #' @examples
 #' #plot(add_to_monophyletic(Tree, "Invent_inventata"))
 add.to.monophyletic <- function(tree, new.tip){
-
+  new.tree<- tree
   new.tip <- gsub(" ", "_", new.tip)
 
   genus <- randtip::firstword(new.tip)
@@ -14,7 +14,7 @@ add.to.monophyletic <- function(tree, new.tip){
       stop(paste0("Genus ", genus, " is not included in your tree."))
   }
 
-  mrca <- phytools::findMRCA(tree = tree, tips = taxa.vector)
-  new.tree <- add.into.node(tree = tree, node = mrca, new.tip = new.tip)
+  mrca <- phytools::findMRCA(tree = new.tree, tips = taxa.vector)
+  new.tree <- add.into.node(tree = new.tree, node = mrca, new.tip = new.tip)
   return(new.tree)
 }
