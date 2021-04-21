33e41a3fc10bbaa2c86cdc7857433a5ac071498e
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-04-21 17:03:49 +0200

    binding corrections

--- R/add_over_node.R
+++ R/add_over_node.R
@@ -1,7 +1,7 @@
 #' Function to add tips over a node. Valid for "sibling" species or genera.
 #' @export
 add.over.node <- function(tree, new.tip, node){
-    df <- data.frame("parent"=tree$edge[1], "node"=tree$edge[1], "length"=tree$edge.length, "id"=1:length(tree$edge.length))
+    df <- data.frame("parent"=tree$edge[,1], "node"=tree$edge[,2], "length"=tree$edge.length, "id"=1:length(tree$edge.length))
 
     to.index <- randtip::get.index(tree, node = node)
     df <- df[df$id==to.index,]
