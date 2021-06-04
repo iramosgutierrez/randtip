ab810175e0502e9fbd7dc1b68096c265cb1b3e04
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-06-04 15:52:45 +0200

    name change and plot function erased

--- R/visualize_tree.R
+++ R/visualize_tree.R
@@ -1,27 +1,6 @@
-#' Function to plot tree cut from MDCC
-#' @export
-plotTree.MDCC<- function(tree, DF1, MDCC.info=list("level"=NA, "MDCC"=NA), DF2=NULL){
-
-  level<-MDCC.info$level
-  MDCC<- MDCC.info$MDCC
-
-  spss<- DF1[which(DF1[,level]==MDCC),]
-  if(!is.null(DF2)){spss2<- DF2[which(DF2[,level]==MDCC),]}
-  genera<- unique(spss$genus)
-  if(!is.null(DF2)){genera<- unique(c(spss$genus, spss2$genus)) }
-  cut.list<- tree$tip.label[randtip::firstword(tree$tip.label)%in% genera]
-  if(length(cut.list)==0){stop("Specified MDCC is not reflected in the tree!")}
-  if(length(cut.list)==1){stop("Specified MDCC is related to just 1 tree tip!")}
-  cut.node<- ape::getMRCA(tree, tip =cut.list )
-  subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
-  return(plot(subtree))
-}
-#plotTree.MDCC(tree25, DF1 =DF1, MDCC="Pinaceae")
-
-
 #' Function to obtain tree cut from MDCC
 #' @export
-cutTree.MDCC<- function(tree, DF1, MDCC.info=list("level"=NA, "MDCC"=NA), DF2=NULL){
+split.MDCC<- function(tree, DF1, MDCC.info=list("level"=NA, "MDCC"=NA), DF2=NULL){
 
   level<-MDCC.info$level
   MDCC<- MDCC.info$MDCC
@@ -37,4 +16,4 @@ cutTree.MDCC<- function(tree, DF1, MDCC.info=list("level"=NA, "MDCC"=NA), DF2=NU
   subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
   return((subtree))
 }
-#cutTree.MDCC(tree, DF1, MDCC="Clinopodium")
+
