00bfb66a7ce1cc09a3d1df7bbf3467fe0faf9356
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-06-09 16:45:27 +0200

    Update utils.R
    bracket error in find mdcc

--- R/utils.R
+++ R/utils.R
@@ -389,12 +389,12 @@ usingMDCCfinder<- function(DF1, taxon=NULL, tree, verbose=F){
       if(is.na(MDCC)){MDCC.lev.vect[v]<-NA}
 
 
-    }
+
     if(is.na(MDCC.vect[v])|is.na(MDCC.lev.vect[v])){MDCC.phyletictype.vect[v]<-NA}else{
       MDCC.phyletictype.vect[v]<-randtip::MDCC.phyleticity(DF1 = DF1, tree =  tree,
                                                            MDCC.info = list(level=MDCC.lev.vect[v], MDCC=MDCC.vect[v]), trim = T)
     }
-  }
+  }}
 
 
   return(list(MDCC=MDCC.vect,MDCC.levels=MDCC.lev.vect, MDCC.phylstat=MDCC.phyletictype.vect) )
