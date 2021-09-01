aa6bad9c3ea66d5cd7a1b337e638f7687e9a16f0
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-09-01 16:31:19 +0200

    DF2 eliminated in visualize.tree
    info based only in DF1

--- R/visualize_tree.R
+++ R/visualize_tree.R
@@ -1,21 +1,20 @@
 #' Function to obtain tree cut from MDCC
 #' @export
-getClade<- function(tree, DF1, cladeinfo=list("level"=NA, "clade"=NA), DF2=NULL){
+getClade<- function(tree, DF1, cladeinfo=list("level"=NA, "clade"=NA)){
 if(is.null(names(cladeinfo))){names(cladeinfo)<- c("level", "clade")}
 
   level<-cladeinfo$level
   clade<- cladeinfo$clade
 
   spss<- DF1[which(DF1[,level]==clade),]
-  if(!is.null(DF2)){spss2<- DF2[which(DF2[,level]==clade),]}
   genera<- unique(spss$genus)
-  if(!is.null(DF2)){genera<- unique(c(spss$genus, spss2$genus)) }
+
   cut.list<- tree$tip.label[randtip::firstword(tree$tip.label)%in% genera]
   if(length(cut.list)==0){stop("Specified clade is not reflected in the tree!")}
   if(length(cut.list)==1){stop("Specified clade is related to just 1 tree tip!")}
   cut.node<- ape::getMRCA(tree, tip =cut.list )
   subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
-  return(list("Tree"=subtree, "DF1"=DF1, "level"=level, "clade"=clade, "DF2"=DF2))
+  return(list("Tree"=subtree, "DF1"=DF1, "level"=level, "clade"=clade))
 }
 
 clade_col <- function(getClade.output, sharingtaxa.col="green",
@@ -25,18 +24,13 @@ clade_col <- function(getClade.output, sharingtaxa.col="green",
   level <- getClade.output$level
   clade <- getClade.output$clade
   DF1   <- getClade.output$DF1
-  DF2   <- getClade.output$DF2
 
   spss<- DF1[which(DF1[,level]==clade),]
-  if(!is.null(DF2)){spss2<- DF2[which(DF2[,level]==clade),]}
   genera<- unique(spss$genus)
-  if(!is.null(DF2)){genera<- unique(c(spss$genus, spss2$genus)) }
 
 
   intruders<- DF1[which(DF1[,level]!=clade),]
-  if(!is.null(DF2)){intruders2<- DF2[which(DF2[,level]!=clade),]}
   intrudergenera<- unique(intruders$genus)
-  if(!is.null(DF2)){intrudergenera<- unique(c(intruders$genus, intruders2$genus)) }
 
   colours<- vector("character", length(CladeTree$tip.label))
   colours[]<- stowaway.col
