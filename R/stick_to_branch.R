edcea4145653ea616be35a8acd13e07c1b7c78ca
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-09-14 15:33:15 +0200

    stick to branchrewritten
    stick to branch re-done and auxiliar function

--- R/stick_to_branch.R
+++ R/stick_to_branch.R
@@ -1,104 +1,68 @@
-get.interm.edges <- function(tree, tips1, tips2, get.length = TRUE){
-
-    if(get.length){
-        df <- data.frame(tree$edge, tree$edge.length)
-    }
-    length.sum <- NULL
-    
-    node1 <- phytools::findMRCA(tree, tips = tips1, "node")
-
-    if(any(duplicated(tips2))) {
-        child <- which(tree$tip.label == tips2[1])
-    } else{
-        child <- phytools::findMRCA(tree, tips = tips2, "node")
-    }
-
-    parent <- phytools::getParent(tree, child) 
-    bind.edges <- matrix(c(parent, child), nrow = 1, ncol = 2)
-    colnames(bind.edges) <- c("parent", "child")
-
-    if(tree$edge[ape::which.edge(tree, child), 1] == node1) {
-        length.sum <- df[ape::which.edge(tree, child), 3] 
-    }
-    while(parent != node1) {
-        desc.par <- phytools::getDescendants(tree, parent, curr = NULL)
-        desc.child <- phytools::getDescendants(tree, child, curr = NULL)
-        desc.par <- c(desc.par[!(desc.par %in% desc.child) & desc.par != child], 
-                      parent)
-        new.edges.loc <- (tree$edge[, 1] %in% desc.par) & (tree$edge[, 2] != child)
-        bind.edges <- rbind(bind.edges, tree$edge[new.edges.loc, ])
-        child <- parent
-        parent <- phytools::getParent(tree, parent)
-        bind.edges <- rbind(bind.edges, c(parent, child))
-    }
-    if(get.length & is.null(length.sum)){
-        length.sum <- sum(df[df[,1] %in% bind.edges[,1] & 
-                             df[,2] %in% bind.edges[,2], 3])
-    }
-    
-    indexes <- vector(mode = "numeric", length = nrow(bind.edges))
-    for(j in 1:nrow(bind.edges)){
-        indexes[j] <- ape::which.edge(tree, bind.edges[j,2]) 
-    }
-
-    return(list(bind.edges = bind.edges,
-                indexes = indexes,
-                length.sum = length.sum))
-
-}
-
-#' Function to add species at specified branches. 
-#' @param edges matrix with 4 columns character vectors. 
-#'        Columns 1 and 2 define the parent node of candidate branches and 
-#'        columns 3 and 4 define the corresponding child node 
-#'        (mrca of species pairs). Note that a pair of nodes may not 
+#' Function to add species at specified branches.
+#' @param edges matrix with 4 columns character vectors.
+#'        Columns 1 and 2 define the stem node of candidate branches and
+#'        columns 3 and 4 define the crown node
+#'        (mrca of species pairs). Note that a pair of nodes may not
 #'        necessarily define one single branch but a set of them
-#' @export 
-stick.to.branch <- function(tree, edges, new.tip = new.tip, prob = TRUE){
+#' @export
+stick.to.branch <- function(tree, edges, new.tip, rand.type="random"){
 
-    df <- data.frame(tree$edge, tree$edge.length)
-    
-    edges <- cbind(edges, 1)
-    if(prob){
+    df <- data.frame("parent"=tree$edge[,1], "node"=tree$edge[,2],
+                     "length"= tree$edge.length, "id"=1:length(tree$edge[,1]) )
+    permittednodes<- as.numeric(NULL)
+    root<- randtip::findRoot(tree)
+    edges[,1]<- gsub(" ", "_", edges[,1])
+    edges[,2]<- gsub(" ", "_", edges[,2])
+    edges[,3]<- gsub(" ", "_", edges[,3])
+    edges[,4]<- gsub(" ", "_", edges[,4])
 
-        relativ_probs <- tree$edge.length
-        for(i in 1:nrow(edges)) {
-            tips1.char = edges[i,1:2]
-            tips2.char = edges[i,3:4]
-            interm.edges <- get.interm.edges(tree, tips1.char, tips2.char,
-                                             get.length = TRUE)
-            edges[i, 5] <- interm.edges$length.sum
-        }
-    }else{
-        relativ_probs <- rep(1, length(tree$edge.length))
+
+    for(i in 1:nrow(edges)){
+      if(!all(c(edges[i,1],edges[i,2],edges[i,3],edges[i,4])%in%tree$tip.label)){
+        message("Row ", i, " has species not included in the tree and will not be used.")
+        next}
+
+    if(edges[i,1]==edges[i,2]){basenode<- which(tree$tip.label==edges[i,1])}else{
+      basenode<- ape::getMRCA(tree, c(edges[i,1], edges[i,2]))}
+
+    if(edges[i,3]==edges[i,4]){parnode<- which(tree$tip.label==edges[i,3])}else{
+      parnode<- ape::getMRCA(tree, c(edges[i,3], edges[i,4]))}
+
+    if(parnode==basenode){
+      message("Row ", i, " specifies a unique node and not a branch, so it will not be used.")
+      next}
+
+    perm.nodes.i<- as.numeric(NULL)
+    n<- basenode
+    p<- parnode
+    while(n!=p){
+      if(n==root){
+        message("Row ", i, " does not reflect a set of branches, so it will not be used.")
+        perm.nodes.i<- NULL
+        break}
+      perm.nodes.i<- c(perm.nodes.i, n)
+      n<-df$parent[df$node==n]
+
+
     }
 
-    edge.selec <- sample(1:nrow(edges), 1, prob = edges[,5])
-    tips1.char <- edges[edge.selec,1:2]
-    tips2.char <- edges[edge.selec,3:4]
-
-    interm.edges <- get.interm.edges(tree, tips1.char, tips2.char)
-    
-    if(length(interm.edges$indexes) == 1){
-        to.index <- interm.edges$indexes
-    }else{
-        to.index <- sample(interm.edges$indexes, 1, 
-                           prob = relativ_probs[interm.edges$indexes])      
+    permittednodes<- c(permittednodes, perm.nodes.i)
     }
+    if(length(permittednodes)==0){stop("No branches could be selected")}
+    permittednodes<- unique(permittednodes)
 
-    bind.where <- tree$edge[to.index, 2]
+    df<- df[df$node%in%permittednodes,]
 
-    tip.pos <- bind.tip.pos(pos.min = 0, 
-                            pos.max = tree$edge.length[to.index])
-    
-    tree <- phytools::bind.tip(tree, 
-                               new.tip, 
-                               edge.length = NULL, 
-                               where = bind.where, 
-                               position = tip.pos)
+    nd<-sample(df$node, 1, prob=df$length)
+    bp<-randtip::binding.position(tree, node = nd, insertion = rand.type)
+    new.tree<- phytools::bind.tip(tree, new.tip, bp$length, bp$where, bp$position)
 
-    return(tree = tree)
+    return(new.tree)
 }
+
+
+
+
 
 
 #' Function to help users visualize the candidate branches for tip insertion with
@@ -106,31 +70,57 @@ stick.to.branch <- function(tree, edges, new.tip = new.tip, prob = TRUE){
 #' @export
 #' @examples
 #' set.seed(1)
-#' tr <- ape::rcoal(25)
-#' edges <- rbind(c("t22", "t14", "t22", "t22"),
-#'                c("t7", "t10", "t11", "t3"))
-#' candidate.edges <- get.candidate.edges(tr, edges)
-#' # Now plot tree highlighting the candidate edges
-#' edge_col <- ifelse(1:nrow(tr$edge) %in% candidate.edges, "red", "black")
-#' edge_wid <- ifelse(1:nrow(tr$edge) %in% candidate.edges, 2, 1)     
-#' plot(tr, edge.color = edge_col, edge.width = edge_wid)
-#' # Using the same tree object tr we can select multiple candidate branches
-#' edges <- rbind(c("t22", "t14", "t1", "t11"))
-#' candidate.edges <- get.candidate.edges(tr, edges)
-#' # Now plot tree highlighting the candidate edges
-#' edge_col <- ifelse(1:nrow(tr$edge) %in% candidate.edges, "red", "black")
-#' edge_wid <- ifelse(1:nrow(tr$edge) %in% candidate.edges, 2, 1)     
-#' plot(tr, edge.color = edge_col, edge.width = edge_wid)
-get.candidate.edges <- function(tree, edges){
-    candidate.edges <- c()
-    for(i in 1:nrow(edges)) {            
-        tips1.char = edges[i,1:2]
-        tips2.char = edges[i,3:4]
-        interm.edges <- get.interm.edges(tree, tips1.char, tips2.char,
-                                         get.length = TRUE)
-        
-        candidate.edges <- c(candidate.edges, interm.edges$indexes)
+permitted.branches_col<- function(tree, edges,  rand.type="random",
+                              permitted.col="red", forbidden.col="black"){
+
+  df <- data.frame("parent"=tree$edge[,1], "node"=tree$edge[,2],
+                   "length"= tree$edge.length, "id"=1:length(tree$edge[,1]) )
+  permittednodes<- as.numeric(NULL)
+  root<- randtip::findRoot(tree)
+  edges[,1]<- gsub(" ", "_", edges[,1])
+  edges[,2]<- gsub(" ", "_", edges[,2])
+  edges[,3]<- gsub(" ", "_", edges[,3])
+  edges[,4]<- gsub(" ", "_", edges[,4])
+
+
+  for(i in 1:nrow(edges)){
+    if(!all(c(edges[i,1],edges[i,2],edges[i,3],edges[i,4])%in%tree$tip.label)){
+      message("Row ", i, " has species not included in the tree and will not be used.")
+      next}
+
+    if(edges[i,1]==edges[i,2]){basenode<- which(tree$tip.label==edges[i,1])}else{
+      basenode<- ape::getMRCA(tree, c(edges[i,1], edges[i,2]))}
+
+    if(edges[i,3]==edges[i,4]){parnode<- which(tree$tip.label==edges[i,3])}else{
+      parnode<- ape::getMRCA(tree, c(edges[i,3], edges[i,4]))}
+
+    if(parnode==basenode){
+      message("Row ", i, " specifies a unique node and not a branch, so it will not be used.")
+      next}
+
+    perm.nodes.i<- as.numeric(NULL)
+    n<- basenode
+    p<- parnode
+    while(n!=p){
+      if(n==root){
+        message("Row ", i, " does not reflect a set of branches, so it will not be used.")
+        perm.nodes.i<- NULL
+        break}
+      perm.nodes.i<- c(perm.nodes.i, n)
+      n<-df$parent[df$node==n]
+
+
     }
-    
-    return(unique(candidate.edges))
+
+    permittednodes<- c(permittednodes, perm.nodes.i)
+  }
+  if(length(permittednodes)==0){stop("No branches could be selected")}
+  permittednodes<- unique(permittednodes)
+
+  col <- vector(mode = "character", length(tree$edge[,1]))
+  col[1:length(col)] <- forbidden.col
+  col[df$node%in%permittednodes]<- permitted.col
+
+  return(col)
 }
+
