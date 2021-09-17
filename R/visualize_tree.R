a41bc5fd89089f293ced8774fdbd6603f53074c8
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-09-17 11:06:54 +0200

    plot.clade function implemented
    plot.clade implemented for easy use

--- R/visualize_tree.R
+++ R/visualize_tree.R
@@ -14,13 +14,27 @@ get.clade<- function(tree, DF0, level, clade){
   return(list("Tree"=subtree, "DF0"=DF0, "level"=level, "clade"=clade))
 }
 
-clade.col <- function(get.clade.output, sharingtaxa.col="green",
-                      intruder.col="red",stowaway.col="black"){
+#' Function to plot subtree
+#' @export
+plot.clade<- function(get.clade.out, corankedtaxa.col="#03C03C",
+                      intruder.col="#C23B23",stowaway.col="black", ...){
+  if(!(is.list(get.clade.out)|all(names(get.clade.out)==c("Tree","DF0","level","clade")))){
+    stop("Please feed this function with the returned object from get.clade function")
+  }
 
-  CladeTree<-get.clade.output$Tree
-  level <- get.clade.output$level
-  clade <- get.clade.output$clade
-  DF0   <- get.clade.output$DF0
+  edgecol<- randtip::clade.col(get.clade.out, corankedtaxa.col=corankedtaxa.col,
+                               intruder.col=intruder.col, stowaway.col=stowaway.col)
+  return(ape::plot.phylo(get.clade.out$Tree, edge.color = edgecol, ...))
+}
+
+
+clade.col <- function(get.clade.out, corankedtaxa.col,
+                      intruder.col,stowaway.col){
+
+  CladeTree<-get.clade.out$Tree
+  level <- get.clade.out$level
+  clade <- get.clade.out$clade
+  DF0   <- get.clade.out$DF0
 
   spss<- DF0[which(DF0[,level]==clade),]
   genera<- unique(spss$genus)
@@ -32,7 +46,7 @@ clade.col <- function(get.clade.output, sharingtaxa.col="green",
   colours<- vector("character", length(CladeTree$tip.label))
   colours[]<- stowaway.col
   colours[randtip::firstword(CladeTree$tip.label)%in%intrudergenera]<- intruder.col
-  colours[randtip::firstword(CladeTree$tip.label)%in%        genera]<- sharingtaxa.col
+  colours[randtip::firstword(CladeTree$tip.label)%in%        genera]<- corankedtaxa.col
 
   return(colours)
 
