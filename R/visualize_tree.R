aea6efea17cbe9236f6a61ccc564cda172048032
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2021-09-20 16:37:40 +0200

    getclade update
    rank automatically found; complete tree extractable

--- R/visualize_tree.R
+++ R/visualize_tree.R
@@ -1,6 +1,16 @@
 #' Function to obtain tree cut from MDCC
 #' @export
-get.clade<- function(tree, info, rank, clade){
+get.clade<- function(tree, info, clade){
+
+
+  rankDF<-info[,c("taxon", randtip::randtip_ranks())]
+
+  rankDF.withclade<-as.data.frame(rankDF[,]==clade)
+  rankDF.withclade<- as.vector(colSums(rankDF.withclade, na.rm = T))
+  ranks<- which(rankDF.withclade>0)
+  if(length(ranks)==0){stop("Specified clade is not reflected in the tree!")}
+  if(length(ranks)> 1){stop("Specified clade reflects several ranks. Please correct your 'info' data frame!")}
+  rank<-names(rankDF)[ranks]
 
 
   spss<- info[which(info[,rank]==clade),]
@@ -10,6 +20,10 @@ get.clade<- function(tree, info, rank, clade){
   if(length(cut.list)==0){stop("Specified clade is not reflected in the tree!")}
   if(length(cut.list)==1){stop("Specified clade is related to just 1 tree tip!")}
   cut.node<- ape::getMRCA(tree, tip =cut.list )
+  if(length(cut.list)==1){stop("Specified clade is related to just 1 tree tip!")}
+  if(cut.node==randtip::findRoot(tree)){
+    return(list("Tree"=tree, "info"=info, "rank"=rank, "clade"=clade))}
+
   subtree<-  phytools::splitTree(tree, split = list("node"=cut.node, "bp"=0))[[2]]
   return(list("Tree"=subtree, "info"=info, "rank"=rank, "clade"=clade))
 }
