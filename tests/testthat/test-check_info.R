9d033ede0c5e93bac40f88f37628530a12d48268
iramosgutierrez <76472476+iramosgutierrez@users.noreply.github.com>
2023-05-16 13:13:08 +0200

    Update test-check_info.R
    parallelize = F in tests

--- tests/testthat/test-check_info.R
+++ tests/testthat/test-check_info.R
@@ -1,15 +1,15 @@
 
 test_that("There may be misspelling errors", {
 
-  expect_message(check_info(mythology$info.list, mythology$back.tree), "TYPO")
-  expect_message(check_info(mythology$info.list, mythology$back.tree), "same taxon")
-  expect_message(check_info(mythology$info.list, mythology$back.tree), "not ultrametric")
+  expect_message(check_info(mythology$info.list, mythology$back.tree), parallelize = F, "TYPO")
+  expect_message(check_info(mythology$info.list, mythology$back.tree), parallelize = F, "same taxon")
+  expect_message(check_info(mythology$info.list, mythology$back.tree), parallelize = F, "not ultrametric")
   
 })
 
 test_that("Phyleticity of groups", {
   
-  checkinfo <- check_info(mythology$info.list, mythology$back.tree)
+  checkinfo <- check_info(mythology$info.list, mythology$back.tree, parallelize = F)
   expect_equal(as.character(checkinfo[checkinfo$taxon=="Gorgona_medusii","Typo" ]), "TRUE")
   expect_equal(checkinfo[checkinfo$taxon=="Gorgona_medusii","Typo.names" ], "Gorgona_medusi")
 
@@ -19,12 +19,12 @@ test_that("Phyleticity of groups", {
 test_that("Edit info", {
   
   info2 <- edit_info(mythology$info.list, "Gorgona_medusii", "taxon", "Gorgona_medusi" )
-  checkinfo <- check_info(info2, mythology$back.tree)
+  checkinfo <- check_info(info2, mythology$back.tree, parallelize = F)
   expect_equal(as.character(checkinfo[checkinfo$taxon=="Gorgona_medusi","Typo" ]), "FALSE")
 })
 
 test_that("Edit tree", {
     tree2 <- edit_tree(mythology$back.tree, "Gorgona_medusi", "Gorgona_medusii" )
-  checkinfo <- check_info(mythology$info.list, tree2)
+  checkinfo <- check_info(mythology$info.list, tree2, parallelize = F)
   expect_equal(as.character(checkinfo[checkinfo$taxon=="Gorgona_medusii","Typo" ]), "FALSE")
 })
